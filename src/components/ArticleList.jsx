d3442dc71380d199abaaabdcb65b81bd1b0e0398
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-05-11 12:17:50 +0300

    added App.jsx inside components folder

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,13 @@
+import Article from "./Article"
+
+    function ArticleList(props) {
+    return (
+        <main>
+            {props.posts.map(post=> (
+                <Article key= {post.id} title={post.title} date={post.date} preview={post.preview} />
+            ))}
+        </main>
+    )
+}
+
+export default ArticleList
